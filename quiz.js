b3864420029f3ba5ad377061442d9cd338927857
JamesonStansfield <159183491+JamesonStansfield@users.noreply.github.com>
2024-03-21 13:27:57 -0400

    final...

--- quiz.js
+++ quiz.js
@@ -9,9 +9,9 @@ const prompt = require('prompt-sync')();
     which was converted using the math formula. Lastly, I logged the conversion
     to the console for the user to see.
 */
-//let tempPrompt = prompt('What is the tempurature in fahrenheit?');
-//let tempCel = (tempPrompt - 32) * 5 / 9
-//console.log("The conversion of your tempurature to Celsius is " + tempCel + ".");
+let tempPrompt = prompt('What is the tempurature in fahrenheit?');
+let tempCel = (tempPrompt - 32) * 5 / 9
+console.log("The conversion of your tempurature to Celsius is " + tempCel + ".");
 
 //Program 2: Grade Calculator
 
@@ -34,4 +34,29 @@ else if(gradePrompt < 60) {
 }
 //Program 3: Leap Year Checker
 
+
 //Program 4: Largest Number Finder
+
+let num1 = prompt('What is the first number?');
+let num2 = prompt('What is the second number?');
+let num3 = prompt('What is the third number?');
+
+if (num1 > num2) {
+    let largestNum = num1
+}
+else if (num1 > num3) {
+    let largestNum = num1
+}
+else if (num2 > num1) {
+    let largestNum = num2
+}
+else if (num2 > num3) {
+    let largestNum = num2
+}
+else if (num3 > num1) {
+    let largestNum = num3
+}
+else if (num3 > num2) {
+    let largestNum = num3
+}
+console.log('The largest number is ' + largestNum + '.');
