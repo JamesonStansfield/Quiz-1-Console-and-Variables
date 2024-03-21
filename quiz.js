69dd95d1363bf0862dc4ae465ddc1d65212ef982
JamesonStansfield <159183491+JamesonStansfield@users.noreply.github.com>
2024-03-21 13:20:34 -0400

    commmit 2/

--- quiz.js
+++ quiz.js
@@ -2,8 +2,36 @@ const prompt = require('prompt-sync')();
 
 //Program 1: Temperature Converter
 
+/*
+    I used the prompt for the user to imput their tempurature.
+    The fahrenheit tempurature is saved under the variable "tempPrompt"
+    Then, I created the variable "tempCel" to hold the celcius tempurature
+    which was converted using the math formula. Lastly, I logged the conversion
+    to the console for the user to see.
+*/
+//let tempPrompt = prompt('What is the tempurature in fahrenheit?');
+//let tempCel = (tempPrompt - 32) * 5 / 9
+//console.log("The conversion of your tempurature to Celsius is " + tempCel + ".");
+
 //Program 2: Grade Calculator
 
+let gradePrompt = prompt("What is Your grade? (write just the number, no percentage)");
+
+if (gradePrompt > 90) {
+    console.log('You have an A!');
+}
+else if(gradePrompt > 80) {
+    console.log('You have a B!');
+}
+else if(gradePrompt > 70) {
+    console.log('You have a C!');
+}
+else if(gradePrompt > 60) {
+    console.log('You have a D!');
+}
+else if(gradePrompt < 60) {
+    console.log('You have a F!');
+}
 //Program 3: Leap Year Checker
 
 //Program 4: Largest Number Finder
